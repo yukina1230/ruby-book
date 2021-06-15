4e42bfdbcfe220e52ff7a34e7e3fbc5a8162f941
yukina1230 <lost.laugh1230@gmail.com>
2021-06-15 13:42:01 +0900

    [Add]rgb変換プログラム

--- lib/rgb.rb
+++ lib/rgb.rb
@@ -1,7 +1,15 @@
 def to_hex(r, g, b)
-  hex = '#'
-  [r, g, b].each do |n|
+  [r, g, b].inject('#') do |hex, n|
     hex += n.to_s(16).rjust(2, '0')
   end
-  hex
+end
+
+def to_ints(hex)
+  r = hex[1..2]
+  g = hex[3..4]
+  b = hex[5..6]
+  ints = []
+  [r, g, b].map do |s|
+    s.hex
+  end
 end
