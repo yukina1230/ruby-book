53a120a7ad3e04fd2c79202803684a7a642dc4fc
yukina1230 <lost.laugh1230@gmail.com>
2021-06-22 10:56:02 +0900

    [Finish]convert_hash_syntax

--- lib/convert_hash_synax.rb
+++ lib/convert_hash_synax.rb
@@ -1,3 +1,3 @@
 def convert_hash_synax(old_synax)
-  old_synax
+  old_synax.gsub(/:(\w+) *=> */, '\1: ')
 end
--- test/convert_hash_synax_test.rb
+++ test/convert_hash_synax_test.rb
@@ -3,6 +3,20 @@ require './lib/convert_hash_synax'
 
 class ConvertHashSynaxTest < Minitest::Test
   def test_convert_hash_synax
-    assert_wqual '{}', convert_hash_synax('{}')
+    old_synax = <<~TEXT
+      {
+        :name => 'Alice',
+        :age=>20,
+        :gender  =>  :female
+      }
+    TEXT
+    expected = <<~TEXT
+      {
+        name: 'Alice',
+        age: 20,
+        gender: :female
+      }
+    TEXT
+    assert_equal '{}', convert_hash_synax('{}')
   end
 end
